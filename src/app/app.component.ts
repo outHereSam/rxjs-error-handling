e3a483aa12bc6b9b747a02f02c7441622153a586
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-04 11:56:45 +0000

    Implements of to create an observable and delay to simulate http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './interfaces/IProduct';
+import { products } from '../assets/data';
+import { delay, from, map, of, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'rxjs-error-handling';
+  data: Product[] = products;
+
+  simulateHttpRequest() {
+    // With timer
+    // return timer(2000).pipe(map(() => this.data));
+
+    // with of and delay
+    return of(this.data).pipe(delay(2000));
+  }
+
+  fetchData() {
+    this.simulateHttpRequest().subscribe((response) => console.log(response));
+  }
 }
