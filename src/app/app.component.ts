c9d18d134a58b22d985a3d49773621097394c1ee
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-04 16:29:55 +0000

    Adds a fallback response or rethrow the error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Product } from './interfaces/IProduct';
 import { products } from '../assets/data';
-import { delay, from, map, of, timer } from 'rxjs';
+import {
+  catchError,
+  delay,
+  finalize,
+  from,
+  map,
+  of,
+  retry,
+  tap,
+  throwError,
+  timer,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,17 +24,55 @@ import { delay, from, map, of, timer } from 'rxjs';
 })
 export class AppComponent {
   title = 'rxjs-error-handling';
-  data: Product[] = products;
+  data: Product[] = [];
+  loading: boolean = false;
+  error: string | null = null;
 
   simulateHttpRequest() {
     // With timer
     // return timer(2000).pipe(map(() => this.data));
 
+    const isSuccess = Math.random() > 0.5;
+
     // with of and delay
-    return of(this.data).pipe(delay(2000));
+    return isSuccess
+      ? of(products).pipe(delay(2000))
+      : throwError(() => new Error('Http request failed'))
+          .pipe(delay(2000))
+          .pipe(
+            retry(3),
+            catchError((error) => {
+              if (Math.random() > 0.5) {
+                console.log('Retries exhausted, providing fallback data.');
+                return of([
+                  {
+                    id: 0,
+                    category: 'Fallback',
+                    name: 'Fallback Product',
+                    price: 0,
+                  },
+                ]);
+              } else {
+                this.error = error.message;
+                return throwError(() => error);
+              }
+            }),
+            finalize(() => {
+              this.loading = false;
+            })
+          );
   }
 
   fetchData() {
-    this.simulateHttpRequest().subscribe((response) => console.log(response));
+    this.loading = true;
+    this.data = [];
+    this.simulateHttpRequest().subscribe({
+      next: (data) => {
+        this.data = data;
+        this.loading = false;
+        this.error = null;
+      },
+      error: (error) => (this.error = error.message),
+    });
   }
 }
